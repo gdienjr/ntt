a7b910d69d9dbf73b06cf96362613375e8635650
gdienjr <gdienjr@gmail.com>
2026-08-06 17:55:28 +0800

    Add files via upload

--- sw.js
+++ sw.js
@@ -7,8 +7,8 @@ const CACHE_NAME = 'ntt-teguh-shell-v1';
 const SHELL_FILES = [
   './index.html',
   './manifest.json',
-  './icons/icon-192.png',
-  './icons/icon-512.png'
+  './icon-192.png',
+  './icon-512.png'
 ];
 
 self.addEventListener('install', function(event) {
